3a68cfd43b08d6f198db6191588153e9e78bae7a
Dimitri <matthewgorbachevskyy@gmail.com>
2018-08-19 14:55:41 +0300

    fixed

--- app.py
+++ app.py
@@ -1,47 +1,53 @@
-"""
-Created by Dimitri Gorbachevskii
-"""
-
-
-class Money(object):
-    """docstring for Money"""
-
-    def __init__(self, amount):
-
-        self.amount = amount
-
-    def getBorrow():
-        pass
-
-
 class Person(object):
     """docstring for Person"""
 
     def __init__(self, name):
-
         self.name = name
         self.spent_money = 0
-        self.has_paid = 0
         self.borrow = {}
-        self.spenting_money = 0
+        self.spent = 0
 
     def spending(self, spent_money):
 
         self.spent_money += spent_money
 
     def paid(self, money, *args):
-        self.has_paid = money
+
+        self.spent += money
         self_money = money / len(args)
+
         for Person_obj in args:
             Person_obj.spending(self_money)
+            self.set_borrow(Person_obj, self_money)
 
-            if Person_obj.name != self.name:
-                Person_obj.getBorrow(self_money, self.name)
+    def set_borrow(self, Person_obj, self_money):
 
-    def getBorrow(self, spent_money, name):
+        if Person_obj.name != self.name:
+            if Person_obj.name in self.borrow:
+                if self.borrow.get(Person_obj.name) <= self_money:
 
-        self.spenting_money += spent_money
-        self.borrow.update({name: self.spenting_money})
+                    diff = self_money - self.borrow.get(Person_obj.name)
+                    print("------------")
+                    print(diff, self.borrow.get(Person_obj.name), self_money)
+                    Person_obj.update_borrow(diff, self.name)
+                    self.borrow.update({Person_obj.name: 0})
+
+                elif self.borrow.get(Person_obj.name) > self_money:
+
+                    diff = self.borrow.get(Person_obj.name) - self_money
+                    print("++++++++++++")
+                    print(diff, self.borrow.get(Person_obj.name), self_money)
+                    self.borrow.update({Person_obj.name: diff})
+            else:
+
+                Person_obj.update_borrow(self_money, self.name)
+
+    def update_borrow(self, money, name):
+
+        if self.borrow.get(name):
+            money += self.borrow.get(name)
+
+        self.borrow.update({name: money})
 
 
 Dimas = Person("Dimas")
@@ -49,19 +55,20 @@ Orest = Person("Orest")
 Vitalik = Person("Vitalik")
 
 
-Dimas.paid(10, Dimas, Orest)
-
 Orest.paid(10, Dimas, Orest)
-#Orest.paid(10, Dimas, Orest)
-Vitalik.paid(10, Dimas, Vitalik, Orest)
+Dimas.paid(20, Dimas, Orest)
+Dimas.paid(20, Dimas, Orest)
+Dimas.paid(200, Orest, Vitalik)
+Orest.paid(10, Dimas, Orest)
+Orest.paid(1000, Dimas, Orest, Vitalik)
+Dimas.paid(200, Dimas, Orest)
+Vitalik.paid(10, Dimas, Orest)
+Vitalik.paid(1000, Dimas, Orest)
 
-print(Dimas.spent_money, Dimas.borrow)
-print()
-print(Orest.spent_money, Orest.borrow)
-print()
-print(Vitalik.spent_money, Vitalik.borrow)
-# print(Vitalik.spent_money)
 
-# print(Dimas.has_paid)
-# print(Orest.has_paid)
-# print(Vitalik.has_paid)
+print(Dimas.name, Dimas.spent, Dimas.spent_money, Dimas.borrow)
+print()
+print(Orest.name, Orest.spent, Orest.spent_money, Orest.borrow)
+print()
+print(Vitalik.name, Vitalik.spent, Vitalik.spent_money, Vitalik.borrow)
+print()
